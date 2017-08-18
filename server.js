2deb4f0d485ec500dd659ff0dc07542c2625e0c3
osztenkurden <hubertwalczak8@gmail.com>
2017-08-18 22:59:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -83,18 +83,19 @@ server = http.createServer(function (req, res) {
 var slotted = [];
 var meth = {
 	getPlayers: function(){
-		if(this.info.allplayers) return this.info.allplayers
+		if(this.info.allplayers){
+			return this.info.allplayers
+		}
 		return false;
 	},
 	getCT: function(){
 		var all_players = [];
 		var ret = {
-			team_ct: {},
 			players: []
 		};
-		if(this.map && this.map.team_ct){
+		if(this.info.map && this.info.map.team_ct){
 			ret = $.extend({}, ret, this.info.map.team_ct);
-		}else {
+		} else {
 			return false;
 		}
 		for(var psid in this.getPlayers()){
@@ -103,19 +104,16 @@ var meth = {
 				all_players.push(curpl);
 			}
 		}
-		ret.team_ct = all_players;
+		ret.players = all_players;
 		return ret;
 	},
 	getT: function(){
 		var all_players = [];
 		var ret = {
-			team_t: {},
 			players: []
 		};
-		if(this.map && this.map.team_t){
+		if(this.info.map && this.info.map.team_t){
 			ret = $.extend({}, ret, this.info.map.team_t);
-		} else {
-			return false;
 		}
 		for(var psid in this.getPlayers()){
 			var curpl = this.getPlayers()[psid];
@@ -123,13 +121,18 @@ var meth = {
 				all_players.push(curpl);
 			}
 		}
-		ret.team_ct = all_players;
+		ret.players = all_players;
 		return ret;
 	},
 	getObserved: function(){
 		if(this.info.player.steamid != 1){
 			var csid = this.info.player.steamid;
-			if(this.getPlayers()[csid]) return this.getPlayers()[csid];
+			var cur_player = this.getPlayers()[csid];
+			if(cur_player){
+				cur_player.steamid = csid;
+				return cur_player;
+			}
+			//if(this.getPlayers()[csid]) return this.getPlayers()[csid];
 			return false;
 		}
 		return this.info.player;
@@ -147,8 +150,13 @@ var meth = {
 		if(this.info.round) return this.info.round;
 		return false;
 	},
-	map: function(){ if(this.info.map) return this.info.map;
-	return false;
+	map: function(){
+		if(this.info.map) return this.info.map;
+		return false;
+	},
+	previously: function(){
+		if(this.info.previously) return this.info.previously;
+		return false;
 	}
 }
 var integ = {
@@ -158,6 +166,7 @@ var integ = {
 
 function update(json) {
 	integ.info = json;
+	integ = $.extend({}, meth, integ);
 	if(integ.getPlayers() !== false){
 		for(var k in integ.getPlayers()){
 			slotted[integ.getPlayers()[k].observer_slot] = integ.getPlayers()[k];
@@ -204,6 +213,8 @@ function updatePage(data) {
 	var players = data.getPlayers(); //Array of other players with SteamID as key
 
 	//OBSERVED PLAYER HUD
+
+	//HERE
 
 	//HUD FOR EVERY OTHER PLAYER
 	if(players){
